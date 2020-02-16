c6e96b62eba4b6c5d6f487388e93803be5873133
micha3lvega <micha3lvega@gmail.com>
2020-02-16 15:15:00 -0500

    Inittial commit

---
+++ src/main/java/co/com/nexos/kardex/front/KardexApplication.java
@@ -0,0 +1,13 @@
+package co.com.nexos.kardex.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardexApplication.class, args);
+	}
+
+}
